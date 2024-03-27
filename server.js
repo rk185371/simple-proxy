2712c39eed78b9479a72b0cdbfbb5ee5f61425b9
Cranjis <UITeam.D3@ncr.com>
2024-03-27 11:46:36 -0500

    init

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3333;
+const axios = require('axios');
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors({ origin: '*' }));
+
+app.get('/localhost-proxy', async (req, res) => {
+  const yourApiUrl = 'https://jsonplaceholder.typicode.com/todos/1';
+  const { data } = await axios.get(yourApiUrl);
+  return res.send(data);
+});
+
+app.listen(port, () => {
+  console.log(`Proxy listening on port http://localhost:${port}`);
+});
